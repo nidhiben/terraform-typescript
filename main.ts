376f7c4a989ad4d6276fbaba8ca4e5dd04df0912
Nidhi Dhammani <moiz.nidhi@gmail.com>
2021-11-06 21:21:06 -0700

    using docker provider

--- main.ts
+++ main.ts
@@ -1,32 +1,31 @@
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-import { Image, Container} from './.gen/providers/docker'
-
+import { Construct } from 'constructs'
+import { App, TerraformStack } from 'cdktf'
+import { Container, Image, DockerProvider } from './.gen/providers/docker'
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
-    super(scope, name);
+    super(scope, name)
 
-    const image = new Image(this, 'nginxImage', {
+    new DockerProvider(this, 'default', {})
+
+    const dockerImage = new Image(this, 'nginxImage', {
       name: 'nginx:latest',
-      keepLocally: false
+      keepLocally: false,
     })
 
-    new Container(this, 'nginxContainer', 
-    {
-      image: image.latest,
-      name: 'cdktf-tutorial',
+    new Container(this, 'nginxContainer', {
+      image: dockerImage.latest,
+      name: 'tutorial',
       ports: [
-        { 
-        internal: 80,
-        external: 8000 
-      }
-      ]
+        {
+          internal: 80,
+          external: 8000,
+        },
+      ],
     })
-
   }
 }
 
-const app = new App();
-new MyStack(app, "terraform-typescript");
-app.synth();
+const app = new App()
+new MyStack(app, 'typescript-docker')
+app.synth()
